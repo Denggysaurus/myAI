b0222d08af37181eedcced24836696cdac8d35e8
Evan Deng <83851685+Denggysaurus@users.noreply.github.com>
2025-03-02 13:59:58 -0500

    Update identity.ts
    test

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Evan Deng`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `Warren`;
+export const AI_NAME: string = `[Warren]`;
 export const AI_TONE: string = `Helpful`;
 export const AI_ROLE: string = `Personal Finance Advisor`;
