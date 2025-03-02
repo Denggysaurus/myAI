d94b2eedd3981f70ea1f1ede08016be39736ead2
Evan Deng <83851685+Denggysaurus@users.noreply.github.com>
2025-03-02 15:21:29 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Customized AI by Evan Deng`;
+export const FOOTER_MESSAGE: string = `Poop AI by Evan Deng`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with Warren, Evan Deng's AI assistant.`;
