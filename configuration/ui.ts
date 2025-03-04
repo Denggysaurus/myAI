d984e46d69c20d82fd5c86732119d6a4b88002b5
Evan Deng <83851685+Denggysaurus@users.noreply.github.com>
2025-03-03 22:39:11 -0500

    Update ui.ts
    named my bot

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Poop AI by Evan Deng`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `MoneyMentor`;
 export const PAGE_DESCRIPTION: string = `Chat with Warren, Evan Deng's AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
