be0d720e388c0fea84aed01fe30e6818490e3494
Evan Deng <83851685+Denggysaurus@users.noreply.github.com>
2025-03-04 19:44:46 -0500

    Update identity.ts
    updated Warren's role

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -3,4 +3,4 @@ export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Warren`;
 export const AI_TONE: string = `Helpful`;
-export const AI_ROLE: string = `Personal Finance Advisor`;
+export const AI_ROLE: string = `Trusted Real Estate Advisor`;
