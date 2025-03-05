0e5d3046e8a38ddbef33c55ccae3ef9307ad3473
Evan Deng <83851685+Denggysaurus@users.noreply.github.com>
2025-03-05 10:50:44 -0500

    Update ui.ts
    Updated AI info

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `HomeGuru: Helping You Buy & Sell with Confidence`;
 export const MESSAGE_PLACEHOLDER: string = `How can I help with your home buying or selling journey?`;
-export const FOOTER_MESSAGE: string = `Poop AI by Evan Deng`;
+export const FOOTER_MESSAGE: string = `HomeGuru can make mistakes. Check important info.`;
 export const CLEAR_BUTTON_TEXT: string = `Clear`;
 export const PAGE_TITLE: string = `HomeGuru`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
